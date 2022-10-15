be07d5beccfea81a3809672748c9e4cc55e9bb36
phyboro <phylisboro@gmail.com>
2022-10-14 19:22:31 -0700

    Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,15 +2,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return Always 0 (success)
  */
 
 int main(void)
 {
-	int a;
+	int c;
 
-	for (a = 0; a < 10; a++)
-		printf("%d", a);
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
