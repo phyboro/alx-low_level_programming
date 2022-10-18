6adf67b612b57620eac00e2afd9cee7b5f911b04
phyboro <phylisboro@gmail.com>
2022-10-18 13:19:29 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+
+	r= _islower('H');
+	_putchar(r + 'O');
+	r= _islower('o');
+	_putchar(r + 'O');
+	r= _islower(108);
+	_putchar(r + 'O');
+	_putchar('\n');
+	return (0);
+}
