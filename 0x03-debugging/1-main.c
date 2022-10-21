d9640493bd408d229217db842d5607773da683fd
phyboro <phylisboro@gmail.com>
2022-10-20 23:33:12 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming : (\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
@@ -17,7 +17,8 @@ int main(void)
 	/*{*/
 	/*putchar(i);*/
 	/*}*/
-	printf("Infinite loop avoided! \\o/\n");
+
+	printf("Infinite loop avoided: \\o/\n");
 
 	return (0);
 }
